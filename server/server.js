6a27f9964639724cc039aa72e3bde68bc2bb0726
rodrigotpires01 <rodrigotpires1@gmail.com>
2026-05-28 23:41:57 +0000

    pequeno update

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const connectDB = require('./config/db.js');
 connectDB();
 
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
